edbc62429fc7c065cfd299eb35bf95feb61814ac
curlynux <curlynux@gmail.com>
2022-08-29 11:59:45 +0200

    feed section finished

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -43,32 +43,70 @@ function Home()
         setTimeout(() => bg.remove(), 5000)
     }
 
+    function handleClick(e) {
+        e.preventDefault();
+        console.log(e);
+        console.log('Le lien a été cliqué.');
+      }
+    function createPost() 
+    {
+        const feed = document.getElementById("feed");
+        const post = document.createElement("div");
+        const login = document.createElement("span");
+        const image = document.createElement("img");
+        const text = document.getElementById("#editor")
+
+        var arr = [];
+
+        post.setAttribute("class", "post");
+        login.setAttribute("class", "login");
+
+        while(arr.length < 1)
+        {
+            var r = Math.floor(Math.random() * 1000) + 1;
+            if(arr.indexOf(r) === -1) arr.push(r);
+            console.log(arr[0]);
+            image.src = `https://picsum.photos/id/${arr[0]}/200/300`
+        }
+
+        
+
+        login.innerHTML = "curlynux";
+        feed.appendChild(post);
+        post.appendChild(login);
+        post.appendChild(image);
+        
+        notification()
+    }
+    
     return(
-        <div>
+        <div id="main">
             <Header/>
             <h1>create post</h1>        
-                <div className="flex">
-                    <form>
+            <div className="flex">
+                <form>
                     <div className="mb-4 w-full parent bg-gray-50 rounded-lg border border-gray-200">
                         <div className="py-2 px-4 bg-white rounded-b-lg text">
                             <textarea id="editor" rows="8" className="block px-0 w-full text-sm text-gray-800 bg-white border-0  focus:ring-0 " placeholder="ecrire un post" required></textarea>
                             <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-  <div className="bg-blue-600 h-2.5 rounded-full" style={{width: "45%"}}></div>
-</div>
+                                <div className="bg-blue-600 h-2.5 rounded-full" style={{width: "45%"}}></div>
+                            </div>
                         </div>
                     </div>
                     <div className="notif"></div>
                     <div className="boxbtn">
-                        <button type="submit" onClick={() => notification()} className="items-center publish px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                        <button type="submit" onSubmit={handleClick} onClick={() => createPost()} className="items-center publish px-5 py-2.5 text-sm text-white font-medium text-center rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-red-800">
                             publish post
                         </button>
-                        <button className="bg-transparent image text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded">
+                        <button className="bg-transparent image font-semibold py-2 px-4">
                             Add image
                         </button>
                     </div>
-                    
-                    </form>
-                 </div>
+                    <div id="feed">
+                        <h1>Feed</h1>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
