ddac8651e658ef9d4ce189d09067383f51df33cb
curlynux <curlynux@gmail.com>
2022-08-29 12:19:49 +0200

    remove useless comment

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
                         mot de passe
                     </label>
                     <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="*********"/>
-                    {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
                 </div>
                 <div className="flex items-center justify-between">
                     <button className="hover:bg-red-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
