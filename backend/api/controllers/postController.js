30565ce2ea2754008bd84f54b8ab8c37159e8246
curlynux <curlynux@gmail.com>
2022-09-13 12:48:31 +0200

    post sucessfully created

--- backend/api/controllers/postController.js
+++ backend/api/controllers/postController.js
@@ -8,13 +8,12 @@ app.use(express.urlencoded({extended: false}))
 exports.test = (req, res) => 
 {
     console.log(req.body);
-    const post = new Post({
+    const post = new Post({post: {
         login: req.body.login,
         imageUrl: req.body.imageUrl,
         post_text: req.body.post_text,
         like: req.body.like,
         disLike: req.body.disLike
-    })
+    }})
     post.save().then(() => res.status(201).json({message: "post created"}))
-    
 }
